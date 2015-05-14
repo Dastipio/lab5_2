b3e65e37f695a8bf862a063871e59479c08a9130
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-05-14 16:25:44 +0200

    Refaktoryzacja testu PersonRepositoryIntegrationTest.java Test nie pozostawiał po sobie początkowego stanu,lecz wprowadzał zmiany dlatego konieczne było dodanie instrukcji usuwającej osobę

--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -4,31 +4,33 @@ import static edu.iis.mto.integrationtest.repository.PersonBuilder.person;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
-
 import org.junit.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import edu.iis.mto.integrationtest.model.Person;
-
 
 public class PersonRepositoryIntegrationTest extends IntegrationTest {
 
-	
+	@Autowired
 	private PersonRepository personRepository;
 
 	@Test
-	public void testCanAccessDbAndGetTestData() {
-//		List<Person> foundTestPersons = personRepository.findAll();
-//		assertEquals(2, foundTestPersons.size());
+	public void testSaveNewPersonAndCheckIsPersisted() {
+		long count = personRepository.count();
+
+		Person person = a(person().withId(count + 1).withFirstName("Roberto")
+				.withLastName("Mancini"));
+		personRepository.save(person);
+		assertEquals(count + 1, personRepository.count());
+		assertEquals("Mancini", personRepository.findOne(count + 1)
+				.getLastName());
+
+		personRepository.delete(person);
 	}
 
 	@Test
-	public void testSaveNewPersonAndCheckIsPersisted() {
-//		long count = personRepository.count();
-//		personRepository.save(a(person().withId(count + 1)
-//				.withFirstName("Roberto").withLastName("Mancini")));
-//		assertEquals(count + 1, personRepository.count());
-//		assertEquals("Mancini", personRepository.findOne(count + 1)
-//				.getLastName());
+	public void testCanAccessDbAndGetTestData() {
+		List<Person> foundTestPersons = personRepository.findAll();
+		assertEquals(2, foundTestPersons.size());
 	}
 
 	private Person a(PersonBuilder builder) {

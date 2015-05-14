c32ee1e2092c73c2a11ee3dd17f930ba53ff94d5
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-05-14 16:21:06 +0200

    Skonfigurowanie zawartości projektu

--- src/main/java/edu/iis/mto/integrationtest/config/ApplicationConfig.java
+++ src/main/java/edu/iis/mto/integrationtest/config/ApplicationConfig.java
@@ -1,8 +1,26 @@
 package edu.iis.mto.integrationtest.config;
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
+
+import edu.iis.mto.integrationtest.utils.ModeUtils;
 
 
+@Configuration //wskazuje, ze klasa zawiera konfiguracje bean’ów Spring
+@ComponentScan("edu.iis.mto.integrationtest.repository") //ustala kontekst poszukiwania komponentów
+@Import(value = { PersistenceConfig.class }) //klasa zwiazana z konfiguracja bean’ów, która importujemy
 public class ApplicationConfig {
 
+	@Bean
+	public static PropertyPlaceholderConfigurer configBean(){
+		PropertyPlaceholderConfigurer resolver = new PropertyPlaceholderConfigurer();
+		resolver.setLocation(new ClassPathResource(ModeUtils.getMode().getModeName() + "-persistence.properties"));
+		
+		return resolver;
+	}
 	
 }
